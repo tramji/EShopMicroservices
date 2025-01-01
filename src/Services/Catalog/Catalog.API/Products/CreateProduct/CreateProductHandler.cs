f098ccf157f8a79aac4d8bab5aa5688af0ea16f5
Ram Ji Tiwari <ramjidream1@outlook.com>
2025-01-02 01:26:21 +0530

    Catalog Service

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,26 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Catagory, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Catagory,
+                Description = command.Description,
+                ImageFIle = command.ImageFile,
+                Price = command.Price
+            };
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
